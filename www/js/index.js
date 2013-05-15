174686bfc0f766a2ac15369cc96e9ed4b178488c
Jeremy Perret <MasterOfUniverse@devster.org>
2013-05-15 19:37:47 +0200

    yataaaa test camera

--- www/js/index.js
+++ www/js/index.js
@@ -1,42 +1,82 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     initialize: function() {
-        this.bind();
-    },
-    bind: function() {
-        document.addEventListener('deviceready', this.deviceready, false);
-    },
-    deviceready: function() {
-        // This is an event handler function, which means the scope is the event.
-        // So, we must explicitly called `app.report()` instead of `this.report()`.
-        app.report('deviceready');
-    },
-    report: function(id) {
-        // Report the event in the console
-        console.log("Report: " + id);
+        console.log('initialize');
+        this.log('Ready');
 
-        // Toggle the state from "pending" to "complete" for the reported ID.
-        // Accomplished by adding .hide to the pending element and removing
-        // .hide from the complete element.
-        document.querySelector('#' + id + ' .pending').className += ' hide';
-        var completeElem = document.querySelector('#' + id + ' .complete');
-        completeElem.className = completeElem.className.split('hide').join('');
+        this.pictureSource = isMobile ? navigator.camera.PictureSourceType : null;
+        this.destinationType = isMobile ? navigator.camera.DestinationType : null;
+    },
+
+    log: function(mess) {
+        $('h1').html(mess);
+    },
+
+    // Called when a photo is successfully retrieved
+    onPhotoDataSuccess: function(imageData) {
+        this.log('onPhotoDataSuccess');
+        var $img = $('img', {id: 'photo'});
+
+        $img.attr('src', "data:image/jpeg;base64," + imageData);
+
+        $('#data').html($img);
+    },
+
+    // Called when a photo is successfully retrieved
+    onPhotoURISuccess: function(imageURI) {
+        this.log('onPhotoURISuccess');
+        var $img = $('img', {id: 'photo'});
+
+        $img.attr('src', imageURI);
+
+        $('#data').html($img);
+    },
+
+    // A button will call this function
+    capturePhoto: function() {
+        this.log('capturePhoto');
+        // Take picture using device camera and retrieve image as base64-encoded string
+        navigator.camera.getPicture(this.onPhotoDataSuccess, this.onFail, {
+            quality: 50,
+            destinationType: this.destinationType.DATA_URL
+        });
+    },
+
+    // A button will call this function
+    capturePhotoEdit: function() {
+        this.log('capturePhotoEdit');
+        // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
+        navigator.camera.getPicture(this.onPhotoDataSuccess, this.onFail, {
+            quality: 20, allowEdit: true,
+            destinationType: this.destinationType.DATA_URL
+        });
+    },
+
+    // A button will call this function
+    getPhotoLibrary: function() {
+        this.log('getPhotoLibrary');
+        // Retrieve image file location from specified source
+        navigator.camera.getPicture(this.onPhotoURISuccess, this.onFail, {
+            quality: 50,
+            destinationType: this.destinationType.FILE_URI,
+            sourceType: this.pictureSource.PHOTOLIBRARY
+        });
+    },
+
+    // A button will call this function
+    getPhotoAlbum: function() {
+        this.log('getPhotoAlbum');
+        // Retrieve image file location from specified source
+        navigator.camera.getPicture(this.onPhotoURISuccess, this.onFail, {
+            quality: 50,
+            destinationType: this.destinationType.FILE_URI,
+            sourceType: this.pictureSource.SAVEDPHOTOALBUM
+        });
+    },
+
+    // Called if something bad happens.
+    onFail: function(message) {
+        setTimeout(function() {
+            alert('Failed because: ' + message);
+        }, 0);
     }
 };
